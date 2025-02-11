8f97e1ac85e45eda2dd3e49a2034c5e3c52e076d
Gabriel <gabriel5ini@hotmail.com>
2025-02-10 23:30:51 -0300

    removed system theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default async function RootLayout({
           <ThemeProvider
               attribute="class"
               defaultTheme="dark"
-              enableSystem
               disableTransitionOnChange
           >
             <ClientProviderLayout>{children}</ClientProviderLayout>
