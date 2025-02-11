28c723aee754d6d71c18592c5d0545fa3be1a05e
Gabriel <gabriel5ini@hotmail.com>
2025-02-10 23:18:47 -0300

    feat: now only subcommand or higher can see add officer button

--- src/app/(private)/home/officers/page.tsx
+++ src/app/(private)/home/officers/page.tsx
@@ -1,21 +1,28 @@
 "use client"
 
-import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Plus } from "lucide-react"
 import { OfficersList } from "./_components"
 import { OfficerModal } from "./_components/officer-modal"
+import { RenderPermission } from "../_components"
+import { useSession } from "next-auth/react"
+import { PilotRank } from "@/shared/types"
 
 export default function MembersPage() {
+  const session = useSession()
+  const currentUser = session.data?.user
+
   return (
     <div className="space-y-6">
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">Membros</h1>
-        <OfficerModal>
-          <Button>
-            <Plus className="mr-2 h-4 w-4" /> Adicionar Oficial
-          </Button>
-        </OfficerModal>
+        <RenderPermission user={currentUser} role={PilotRank.SUB_COMMAND} >
+          <OfficerModal>
+            <Button>
+              <Plus className="mr-2 h-4 w-4" /> Adicionar Oficial
+            </Button>
+          </OfficerModal>
+        </RenderPermission>
       </div>
       <OfficersList />
     </div>
